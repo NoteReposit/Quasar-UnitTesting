9ea16cd964dea3903eae6cd7996a1021e3f9c9d3
Yotsakon373 <noteyosakon@gmail.com>
2024-09-06 21:39:08 +0700

    first Quasar Unit-Testing Project

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,49 @@
+<style>
+  .center {
+    margin: auto;
+    width: 60%;
+    border: 3px solid #1976D2;
+    padding: 10px;
+  }
+</style>
+<template>
+  <div class="htmlClass">
+    <h1>{{ title }}</h1>
+    <form class="center">
+      <label>Student ID:</label><br>
+      <input type="text" id="stdID" v-model="stdID" /><br>
+
+      <label>Nick name:</label><br>
+      <input type="text" id="nickName" v-model="nickName" /><br>
+
+      <label>First name:</label><br>
+      <input type="text" id="firstName" v-model="firstName" /><br>
+
+      <label>Last name:</label><br>
+      <input type="text" id="lastName" v-model="lastName" /><br><br>
+      <button @click="submitForm()" class="submitForm">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      title: 'Vue is awesome.',
+      stdID: '',
+      nickName: '',
+      firstName: '',
+      lastName: '',
+    }
+  },
+  methods: {
+    submitForm() {
+      this.$emit('submit form')
+    },
+  }
+}
+</script>
+
+<style scoped></style>
